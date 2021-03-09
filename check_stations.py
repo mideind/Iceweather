52d2d304d17c314c0fbe8dfe9efd06b2675ae299
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-03-09 19:05:27 +0000

    Added util script to check station lookup availability

---
+++ check_stations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+
+from iceweather.stations import STATIONS
+from iceweather import observation_for_station
+
+for s in STATIONS:
+    print("CHecking {0}: {1}".format(s["id"], s["name"]))
+    try:
+        d = observation_for_station(s["id"])
+    except Exception as e:
+        print(e)
+    if d:
+        pprint(d)
+    else:
+        print("ERROR !!!! " + str(s))
